091f5eb3a471c86e834ed6240ac57ce3bd05b1ce
Azarattum <azarattum@pm.me>
2022-12-15 15:24:55 +0700

    Added a home page for demos

---
+++ src/demo/+page.svelte
@@ -0,0 +1,5 @@
+<h2>Demos:</h2>
+<ul>
+  <li><a href="library">Library</a></li>
+  <li><a href="todo">Todo List</a></li>
+</ul>
--- svelte.config.js
+++ svelte.config.js
@@ -11,6 +11,7 @@ const config = {
     adapter: adapter(),
     files: {
       routes: "src/demo",
+      appTemplate: "src/demo/app.html",
     },
   },
 };
