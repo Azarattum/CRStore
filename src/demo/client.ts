b13f1ce330d9b0f5fd9a90cd9240ef84e3b7af97
Azarattum <azarattum@pm.me>
2023-03-01 13:26:04 +0700

    Refactored trpc client connection

--- src/demo/client.ts
+++ src/demo/client.ts
@@ -1,26 +1,23 @@
 import { createTRPCProxyClient, createWSClient, wsLink } from "@trpc/client";
 import type { App } from "./server";
 
-const client = () =>
-  createTRPCProxyClient<App>({
-    links: [
-      wsLink({
-        client: createWSClient({
-          url: "ws://localhost:5173/trpc",
-        }),
-      }),
-    ],
-  });
-
-const proxy: any = new Proxy(() => {}, {
+const proxy = new Proxy(() => {}, {
   apply: () => proxy,
   get: () => proxy,
   set: () => proxy,
-});
+}) as never;
 
 const trpc =
-  !import.meta.env || import.meta.env.SSR
-    ? (proxy as ReturnType<typeof client>)
-    : client();
+  import.meta?.env?.SSR === false
+    ? createTRPCProxyClient<App>({
+        links: [
+          wsLink({
+            client: createWSClient({
+              url: "ws://localhost:5173/trpc",
+            }),
+          }),
+        ],
+      })
+    : proxy;
 
 export { trpc };
