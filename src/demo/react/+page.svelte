942d27bf75c05c1845b743480e4eb12a342477cb
Azarattum <azarattum@pm.me>
2024-01-06 17:24:56 +0700

    Added react demo page

---
+++ src/demo/react/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import ReactDOM from "react-dom/client";
+  import { Component } from "./Component";
+  import { onMount } from "svelte";
+
+  let root: HTMLDivElement;
+  onMount(() => {
+    ReactDOM.createRoot(root).render(Component({}));
+  });
+</script>
+
+<div bind:this={root}></div>
---
+++ src/demo/react/Component.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Component: React.FC = () => {
+  return <h1>123</h1>;
+}
